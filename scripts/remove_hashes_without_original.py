a755553b4434c862e52325e05c9fb3bc2bfbfa50
Steven Van Ingelgem <steven@vaningelgem.be>
2026-09-16 15:48:20 +0200

    style: ruff format and autofix

--- scripts/remove_hashes_without_original.py
+++ scripts/remove_hashes_without_original.py
@@ -10,17 +10,19 @@ for folder in sys.argv[1:]:
         continue
 
     print(f"Loading files in {folder}:")
-    mylist = {p.resolve().absolute() for p in folder.rglob('*.*') if p.is_file()}
+    mylist = {p.resolve().absolute() for p in folder.rglob("*.*") if p.is_file()}
 
     for file in (bar := tqdm(sorted(mylist))):
         bar.set_description(str(file))
 
         # Cleanup hash files without an original one
-        if file.suffix.lower() == '.hash':
+        if file.suffix.lower() == ".hash":
             original_file = Path(str(file)[:-5])
-            if original_file not in mylist and not original_file.exists():  # First check against the list in memory. It'll be faster than going to the filesystem every time.
+            if (
+                original_file not in mylist and not original_file.exists()
+            ):  # First check against the list in memory. It'll be faster than going to the filesystem every time.
                 print(file)
                 # file.unlink()
             continue
 
-    print("")
+    print()
